e1e706023e95fb98fafbe41b48ceffdc750a667b
liyahuan <liyahuan@aragoncs.com>
2022-04-13 15:58:17 +0800

    创建欢迎界面的页面

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { createApp } from 'vue'
 import {App} from './App'
-import {Bar} from './views/Bar'
-import {Foo} from './views/Foo'
-import {createRouter,createWebHashHistory} from 'vue-router'
-const routes = [
-  { path: '/', component: Bar },
-  { path: '/about', component: Foo },
-]
-
-const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: createWebHashHistory(),
-  routes,
-})
+import {createRouter} from 'vue-router'
+import { history } from './shared/history'
+import { routes } from './config/routes'
+const router = createRouter({history, routes})
 
 const app = createApp(App)
-app.use(router)
+app.use(router) 
 app.mount('#app')
 

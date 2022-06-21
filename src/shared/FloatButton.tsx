bbe019654e154fa0573ea61936101d3a72811687
liyahuan <liyahuan@aragoncs.com>
2022-06-21 17:14:39 +0800

    创建button组件

--- src/shared/FloatButton.tsx
+++ src/shared/FloatButton.tsx
@@ -1,18 +1,20 @@
 import { defineComponent, PropType } from 'vue';
-import { Icon, IconName } from './Icon';
+import { Icon,IconName } from './Icon';
 import s from './FloatButton.module.scss';
-export const FloatButton = defineComponent({
-  props: {
+export const FloatButton= defineComponent({
+  props:{
     iconName: {
       type: String as PropType<IconName>,
+      //  加上下面这段话，就必须传iconName,如果不加，也没传值，iconName有可能是undefined
       required: true
     }
   },
-  setup: (props, context) => {
-    return () => (
-      <div class={s.floatButton}>
-        <Icon name={props.iconName} class={s.icon}/>
-      </div>
-    )
-  }
+setup:(props,context)=>{
+return ()=>(
+  <div class={s.floatButton}>
+  {/* name会绑定到use上面，class绑定到svg上面 */}
+    <Icon name={props.iconName} class={s.icon}/>
+  </div>
+  )
+}
 })
--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,21 +1,21 @@
-import { defineComponent, defineProps, PropType } from 'vue';
+import { defineComponent, PropType } from 'vue';
 import s from './Icon.module.scss';
-
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig'
-
-export const Icon = defineComponent({
-  props: {
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'magosteen' | 'pig';
+export const  Icon = defineComponent({
+  // defineComponent 接收大写的Props，就不能用小写的，方方推荐直接用小写的写法如下：
+  // type是传给js,让js知道的类型， proptype是让ts知道的类型，到浏览器中就是js语言了。
+  props:{
     name: {
-      type: String as PropType<IconName>,
-      required: true,
+      type: String  as PropType<IconName>,
+    //  加上下面这段话，就必须传iconName
+      required: true
     }
   },
-  setup: (props, context) => {
-    return () => (
-      <svg class={s.icon}>
-        <use xlinkHref={'#' + props.name}></use>
-      </svg>
+setup:(props,context)=>{
+  return ()=>(
+    <svg class={s.icon}>
+      <use xlinkHref={'#' + props.name}></use>
+    </svg>
     )
   }
 })
-
